433d6003136b4ae15ff27562d4bcf4b3e340b7c2
Robert <robertczeres.pv@gmail.com>
2024-03-01 12:52:35 +0100

    dodanie kodu kalkulatora wieku

---
+++ pierwszy.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+dzisiaj = date.today()
+dzis_rok = dzisiaj.year   
+dzis_miesiac = dzisiaj.month
+dzis_dzien = dzisiaj.day
+
+rok_urodzenia = int(input("Podaj rok urodzenia: ")) 
+
+wiek = dzis_rok - rok_urodzenia
+
+print("Masz " + str(wiek) + " lat")
+
+
+
+
